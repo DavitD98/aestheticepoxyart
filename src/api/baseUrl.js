d7b3db72fc54928df59eb2fceccb1040f53c2293
David Peshtmajyan <100031579+DavitD98@users.noreply.github.com>
2023-06-29 21:23:45 +0400

    Update baseUrl.js

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://frail-wear-elk.cyclic.app"
+const BASE_URL = "https://epoxyart.api.onrender.com"
 //const BASE_URL = "https://epoxyart-api.onrender.com"
 
 export default BASE_URL
