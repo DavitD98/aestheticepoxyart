073ddf8306781c63e7ac9cebc9d7b5684d7e5325
David Peshtmajyan <100031579+DavitD98@users.noreply.github.com>
2023-06-29 21:48:39 +0400

    Update baseUrl.js

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://epoxyart-api-vgug.onrender.com/"
+const BASE_URL = "https://epoxyart-api-vgug.onrender.com"
 //const BASE_URL = "https://epoxyart-api.onrender.com"
 
 export default BASE_URL
